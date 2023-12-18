5661f251d1ee1a279511b4cceca6a3a6c2084ca5
Titouan Massé <titouanmasselaude@gmail.com>
2023-12-18 16:43:40 +0100

    Ajout du connect qui contient les fonctions qui sont répétées (importer comme module) Modification et ajout des premiers bouts de code php dans le fichier temperature.php

---
+++ templates/connect.php
@@ -0,0 +1,20 @@
+<?php
+try{
+
+    $BDD = new PDO('mysql:host=localhost;dbname=cube_meteo','root','');
+
+
+}
+catch(PDOException $erreur){
+    echo 'Echec de la connexion suite à l\'erreur suivante : ' . $erreur->getMessage();
+    exit;
+}
+
+function connect_to($BddName){
+    /* Créer une connection */
+    $BDD = new PDO("mysql:host=localhost;dbname=".$BddName.";charset=utf8",
+    'root',
+    '');
+    return $BDD;
+}
+
