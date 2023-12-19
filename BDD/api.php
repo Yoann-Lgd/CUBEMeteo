927b1322cb9e783d0ea32a82fb35955bc01f9030
Yoann Lgd <yoann.lengrand@icloud.com>
2023-12-19 10:42:41 +0100

    Modification du script de création de la bdd (suppression de la table Raspberry), ajout de la table releves qui va contenir la date, la température, l'humidité. Modification du script qui simule le Raspberry pour ajouter la notion de date. Modification du fichier API, traitement de la date, ajout de Create Read Update de la table sonde

--- BDD/api.php
+++ BDD/api.php
@@ -12,6 +12,7 @@ if ($jsonData !== null) {
     if ($data !== null) {
         $deviceName = $data['deviceName'];
         $temperature = $data['temperature'];
+        $date = $data['date'];
         $humidity = $data['humidity'];
     } else {
         echo "Erreur lors du décodage des données JSON.";
@@ -20,165 +21,62 @@ if ($jsonData !== null) {
     echo "Erreur provenant du scrypt Raspberry";
 }
 
-
-
-// // Fonction pour insérer un Raspberry
-// function insertRaspberry($nom) {
-//     global $BDD;
-
-//     $query = "INSERT INTO Raspberry (Nom) VALUES (:nom)";
-//     $stmt = $BDD->prepare($query);
-//     $stmt->bindParam(':nom', $nom);
-
-//     // Exécute la requête et stocke le résultat dans une variable
-//     $result = $stmt->execute();
-
-//     if ($result) {
-//         echo 'Insertion réussie.';
-//     } else {
-//         echo 'Échec de l\'insertion.';
-//     }
-
-//     return $result;
-// }
-
-// insertRaspberry($deviceName);
-
-// function getRaspberryById($idRaspberry){
-//     global $BDD;
-
-//     $raspberryQuery = "SELECT * FROM Raspberry WHERE ID = :id";
-//     $raspberryStmt = $BDD->prepare($raspberryQuery);
-//     $raspberryStmt->bindParam(':id', $idRaspberry);
-//     $raspberryStmt->execute();
-
-//     return $raspberryStmt->fetch(PDO::FETCH_ASSOC);
-// }
-
-// $raspberry = getRaspberryById(1);
-// print_r($raspberry);
-
 // Fonction pour insérer une Sonde
-function insertSonde($nom, $idRaspberry) {
+function insertSonde($nom) {
     global $BDD;
 
-    $query = "INSERT INTO Sonde (nom, ID_Raspberry) VALUES (:nom, :idRaspberry)";
-
+    $query = "INSERT INTO Sonde (Nom) VALUES (:nom)";
     $stmt = $BDD->prepare($query);
     $stmt->bindParam(':nom', $nom);
-    $stmt->bindParam(':idRaspberry', $idRaspberry);
 
+    // Exécute la requête et stocke le résultat dans une variable
     $result = $stmt->execute();
 
     if ($result) {
-        echo 'Insertion réussie.';
+        echo 'Insertion de la nouvelle sonde réussie.';
     } else {
-        $errorInfo = $stmt->errorInfo();
         echo 'Échec de l\'insertion.';
     }
 
     return $result;
 }
 
+// insertSonde($deviceName);
 
-// insertSonde("NomDeLaSonde", 1);
+// Fonction pour récupérer la sonde selon son id
+function getSondeById($idSonde){
+    global $BDD;
 
-// // Fonction pour lire une Sonde par son ID
-// function getSondeById($id) {
-//     global $BDD;
+    $sondeQuery = "SELECT * FROM Sonde WHERE ID = :id";
+    $sondeStmt = $BDD->prepare($sondeQuery);
+    $sondeStmt->bindParam(':id', $idSonde);
+    $sondeStmt->execute();
 
-//     $query = "SELECT * FROM Sonde WHERE ID = :id";
-//     $stmt = $BDD->prepare($query);
-//     $stmt->bindParam(':id', $id);
-//     $stmt->execute();
-
-//     return $stmt->fetch(PDO::FETCH_ASSOC);
-// }
-// $sonde = getSondeById(1);
-// print_r($sonde)
-
-// // // Fonction pour mettre à jour une Sonde
-// function updateSonde($id, $nom, $idRaspberry) {
-//     global $BDD;
-
-//     $query = "UPDATE Sonde SET nom = :nom, ID_Raspberry = :idRaspberry WHERE ID = :id";
-//     $stmt = $BDD->prepare($query);
-//     $stmt->bindParam(':id', $id);
-//     $stmt->bindParam(':nom', $nom);
-//     $stmt->bindParam(':idRaspberry', $idRaspberry);
-
-//     $result = $stmt->execute();
-
-//     if ($result) {
-//         echo 'Mise à jour réussie.';
-//     } else {
-//         echo 'Échec de la mise à jour.';
-//     }
-
-//     return $result;
-// }
-
-// updateSonde(1, "NouveauNom", 1);
-
-
-// Fonction pour insérer une Température
-//INSERT INTO votre_table (colonne_datetime)
-('2023-01-01 12:30:00');
-function insertTemperature($datetime, $valeur) {
-    global $conn;
-
-    $query = "INSERT INTO Temperature (Date, Valeur) VALUES (:datetime, :valeur)";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(':datetime', $datetime);
-    $stmt->bindParam(':valeur', $valeur);
-
-    return $stmt->execute();
+    return $sondeStmt->fetch(PDO::FETCH_ASSOC);
 }
-// // Fonction pour insérer une Humidité
-// function insertHumidite($date, $heure, $valeur) {
-//     global $conn;
 
-//     $query = "INSERT INTO Humidite (Date, Heure, Valeur) VALUES (:date, :heure, :valeur)";
-//     $stmt = $conn->prepare($query);
-//     $stmt->bindParam(':date', $date);
-//     $stmt->bindParam(':heure', $heure);
-//     $stmt->bindParam(':valeur', $valeur);
+// $sonde = getSondeById(1);
 
-//     return $stmt->execute();
-// }
+// Fonction pour mettre à jour la sonde selon son id
+function updateSonde($id, $nom) {
+    global $BDD;
 
-// // Fonction pour insérer une Analyse
-// function insertAnalyse($idSonde, $idTemperature, $idHumidite) {
-//     global $conn;
+    $query = "UPDATE Sonde SET nom = :nom WHERE ID = :id";
+    $stmt = $BDD->prepare($query);
+    $stmt->bindParam(':nom', $nom);
+    $stmt->bindParam(':id', $id);
 
-//     $query = "INSERT INTO Analyse (ID, ID_Temperature, ID_Humidite) VALUES (:idSonde, :idTemperature, :idHumidite)";
-//     $stmt = $conn->prepare($query);
-//     $stmt->bindParam(':idSonde', $idSonde);
-//     $stmt->bindParam(':idTemperature', $idTemperature);
-//     $stmt->bindParam(':idHumidite', $idHumidite);
+    // Exécute la requête et stocke le résultat dans une variable
+    $result = $stmt->execute();
 
-//     return $stmt->execute();
-// }
+    if ($result) {
+        echo 'Mise à jour de la sonde réussie.';
+    } else {
+        echo 'Échec de la mise à jour.';
+    }
 
-// // Exemple d'utilisation :
+    return $result;
+}
 
-// // Insertion d'une Raspberry
-// insertRaspberry("Raspberry1");
-
-// // Insertion d'une Sonde liée à la Raspberry1
-// $idRaspberry = 1;
-// insertSonde("Sonde1", $idRaspberry);
-
-// // Insertion d'une Température
-// insertTemperature("2023-01-01", "12:00:00", 25.5);
-
-// // Insertion d'une Humidité
-// insertHumidite("2023-01-01", "12:00:00", 60.5);
-
-// // Insertion d'une Analyse liée à la Sonde1, Température et Humidité
-// $idSonde = 1;
-// $idTemperature = 1;
-// $idHumidite = 1;
-// insertAnalyse($idSonde, $idTemperature, $idHumidite);
-
+$sonde = updateSonde(1, "jj");
 ?>
