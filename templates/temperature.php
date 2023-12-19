7cbf9e4bed97691afc124527d0e3244b6f18e547
Pimer0 <paulmerandon@gmail.com>
2023-12-19 10:49:44 +0100

    ajout du chart dans temperature

--- templates/temperature.php
+++ templates/temperature.php
@@ -17,33 +17,52 @@ print_r($dataToReturn);
 
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="../CSS/styles.css" />
-		<link rel="stylesheet" href="../CSS/charts.min.css" />
-		<title>Température</title>
-	</head>
-	<body>
-		<div class="mainTemperature">
-			<div class="temperature">
-				<h1>Quelle température:</h1>
-				<p>Jettez un oeil à la température sur les dernières heures.</p>
-				<div class="graph">
-					<div class="lundi">LU.</div>
-					<div class="mardi">MA.</div>
-					<div class="mercredi">ME.</div>
-					<div class="jeudi">JE.</div>
-					<div class="vendredi">VE.</div>
-					<div class="samedi">SA.</div>
-					<div class="dimanche">DI.</div>
-				</div>
-				<p>La température moyenne sur les 5 derniers jours était de&nbspX.</p>
-			</div>
-		</div>
 
-		<div class="accueil">
-			<a href="index.html"><img src="../images/maison.svg" /></a>
-		</div>
-	</body>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../CSS/styles.css" />
+    <link rel="stylesheet" href="../CSS/charts.min.css" />
+    <title>Température</title>
+</head>
+
+<body>
+    <div class="mainTemperature">
+        <div class="temperature">
+            <h1>Quelle température:</h1>
+            <p>Jettez un oeil à la température sur les dernières heures.</p>
+
+            <table class="charts-css column">
+
+                <caption> Température </caption>
+
+                <tbody>
+                    <tr>
+                        <td style="--size: calc( 40 / 100 )"> </td>
+                    </tr>
+                    <tr>
+                        <td style="--size: calc( 60 / 100 )"> </td>
+                    </tr>
+                    <tr>
+                        <td style="--size: calc( 75 / 100 )"> </td>
+                    </tr>
+                    <tr>
+                        <td style="--size: calc( 90 / 100 )"> </td>
+                    </tr>
+                    <tr>
+                        <td style="--size: calc( 100 / 100 )"> </td>
+                    </tr>
+                </tbody>
+
+            </table>
+
+            <p>La température moyenne sur les 5 derniers jours était de&nbspX.</p>
+        </div>
+    </div>
+
+    <div class="accueil">
+        <a href="index.html"><img src="../images/maison.svg" /></a>
+    </div>
+</body>
+
 </html>
