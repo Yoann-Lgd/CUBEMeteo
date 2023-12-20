ee25a3cdf5dbe5fd3b96c878a821c342f1744f7c
Yoann Lgd <yoann.lengrand@icloud.com>
2023-12-20 23:09:16 +0100

    Suppresion d'Api.php, transfert des méthodes Api.php dans apiRest.php

--- API/apiRest.php
+++ API/apiRest.php
@@ -2,8 +2,9 @@
 
 header("Content-Type: application/json");
 
-require('BDD/RelevesDAO.php');
-require('BDD/SondeDAO.php');
+require('../BDD/RelevesDAO.php');
+require('../BDD/SondeDAO.php');
+require('../scrypt/scrypt.php');
 
 
 // Récupérer la méthode HTTP
@@ -60,4 +61,58 @@ switch ($method) {
         //     break;
 }
 
-echo json_encode($result);
+
+// $jsonData = generateRaspberryData();
+
+// if ($jsonData !== null) {
+//     $data = json_decode($jsonData, true);
+
+//     if ($data !== null) {
+//         $deviceId = $data['id'];
+//         $deviceName = $data['deviceName'];
+//         $temperature = $data['temperature'];
+//         $dateRelevee = $data['date'];
+//         $humidite = $data['humidity'];
+//     } else {
+//         echo "Erreur lors du décodage des données JSON.";
+//     }
+// } else {
+//     echo "Erreur provenant du scrypt Raspberry";
+// }
+
+
+// // Endpoint pour créer une sonde
+// if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom'])) {
+//     $nom = $_POST['nom'];
+
+//     $result = insertSonde($nom);
+
+//     if ($result) {
+//         echo 'Sonde créée avec succès.';
+//     } else {
+//         echo 'Erreur lors de la création de la sonde.';
+//     }
+// } else {
+//     http_response_code(400);
+//     echo 'Requête invalide.';
+// }
+
+
+//Boucle permettant de créer/insérer un jeu de données sur les 5 derniers jours
+function feedRelevesDb(){
+    $jsonData = generateDataForFiveDays();
+
+$dataList = json_decode($jsonData, true);
+
+if ($dataList !== null) {
+    foreach ($dataList as $data) {
+        $temperature = $data['temperature'];
+        $date = $data['date'];
+        $humidite = $data['humidite'];
+        insertReleves($date, $temperature, $humidite, 1);
+    }
+}
+}
+
+
+feedRelevesDb();
