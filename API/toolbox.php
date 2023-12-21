5d627535a0d2cbeb0b63e83356e43efb4ac448b5
Titouan Massé <titouanmasselaude@gmail.com>
2023-12-21 14:53:25 +0100

    déplacement get request dans toolbox

--- API/toolbox.php
+++ API/toolbox.php
@@ -153,5 +153,83 @@ function picto($data){
         echo $coldWarning;
     }
 }
+/////////////////////////////
+/*CONNECTION AVEC LA BDD MYSQL*/
+// $db == database
+
+//Température
+
+if($_GET['combo1']!=""){ //on change les valeur de debut et fin si l'utilisateur les a sélectionnées
+    if($_GET['combo2']!=""){ 
+        $date_debut = $_GET['combo1'];
+        $date_fin = $_GET['combo2'];
+        $array_releves = getRelevesBetweenDates("1", $date_debut, $date_fin);
+    }
+}
+else{
+
+    $date_debut = date("Y-m-d");
+    $date_fin = date("Y-m-d");;
+    // $todayTemp = dayTemp($db,$yesterday);
+    $array_releves = [];
+    // for($i =0;$i<$todayTemp;$i++){
+    //     $array_releves[] = 
+    //         [
+    //         "Temperature" => $todayTemp[0][$i],
+    //         "Date" => date("Y-m-d"),
+    //         ];
+    // }
+
+
+}
+if($_GET['combo1']!="" And $_GET['combo2']==""){
+    $date_debut = date("Y-m-d");
+    $date_fin = date("Y-m-d");
+    $cursor  = $db->query("SELECT Temperature FROM releves WHERE Date ='".$date_debut."'");
+    $data = $cursor->fetchAll(PDO::FETCH_ASSOC);
+    
+    $array_releves = $data;
+    return $array_releves;
+}
+
+
+
+//Humidité
+
+    if($_GET['combo1']!=""){ //on change les valeur de debut et fin si l'utilisateur les a sélectionnées
+    if($_GET['combo2']!=""){ 
+        $date_debut2 = $_GET['combo1'];
+        $date_fin2 = $_GET['combo2'];
+        $array_releves2 = getRelevesBetweenDates("1", $date_debut2, $date_fin2);
+    }
+}
+else{
+
+    $date_debut2 = date("Y-m-d");
+    $date_fin2 = date("Y-m-d");
+    // $todayTemp = dayTemp($db,$yesterday);
+    $array_releves2 = [];
+    // for($i =0;$i<$todayTemp;$i++){
+    //     $array_releves[] = 
+    //         [
+    //         "Temperature" => $todayTemp[0][$i],
+    //         "Date" => date("Y-m-d"),
+    //         ];
+    // }
+
+
+}
+if($_GET['combo1']!="" And $_GET['combo2']==""){
+    $date_debut2 = date("Y-m-d");
+    $date_fin2 = date("Y-m-d");
+    $cursor  = $db->query("SELECT Humidite FROM releves WHERE Date ='".$date_debut2."'");
+    $data = $cursor->fetchAll(PDO::FETCH_ASSOC);
+    
+    $array_releves2 = $data;
+}
+return $array_releves2;
+
+
+// feedRelevesDb();
 
 ?>
