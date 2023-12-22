8761b18e34e91eade83661901c25c96380cbea68
Yoann Lgd <yoann.lengrand@icloud.com>
2023-12-22 02:57:13 +0100

    Test récupération des relevés via une méthode GET et en utilisant l'idSonde présent dans l'url

--- Web/templates/temperature.php
+++ Web/templates/temperature.php
@@ -1,34 +1,55 @@
 <?php
 include_once('toolbox.php');
+///
+if (isset($_GET['idSonde'])) {
+    $idSonde = $_GET['idSonde'];
+} else {
+    echo "Aucun ID n'a été spécifié dans l'URL.";
+}
 
+$apiUrlRelevesSonde = 'http://api.localhost:9530/apirest.php?resource=releves&idSonde=' . $idSonde;
+$ch = curl_init($apiUrlRelevesSonde);
+
+$options = [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+];
+
+curl_setopt_array($ch, $options);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+//////
+
+echo "Données récupérées : <pre>";
+print_r($data);
+echo "</pre>";
 
 $date_debut = $_GET['combo1'];
 $date_fin = $_GET['combo2'];
 
 $post_data = json_encode($data = [
-    'date_debut'=>$date_debut,
-    'date_fin'=>$date_fin]);
+    'date_debut' => $date_debut,
+    'date_fin' => $date_fin,
+]);
 $ch = curl_init();
 
 $options = [
-    CURLOPT_URL=>'http://apimeteo/apiRest.php',
+    CURLOPT_URL => 'http://apimeteo/apiRest.php',
     CURLOPT_POST => 1,
     CURLOPT_POSTFIELDS => $post_data,
     CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_HTTPHEADER => array('Content-Type: application/json')
+    CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
 ];
-curl_setopt_array($ch,$options);
+curl_setopt_array($ch, $options);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
 
-echo "toutes les températures de la période donnée ";
-
-
-
-
-
-
+echo "Toutes les températures de la période donnée ";
 ?>
 
 <!DOCTYPE html>
@@ -47,46 +68,39 @@ echo "toutes les températures de la période donnée ";
         <div class="temperature">
             <h2>Sélectionnez une date :</h2>
             <form method="get">
-            
                 <select name="combo1">
                     <option>2023-12-20</option>
                     <option>2023-12-16</option>
-            
-                    <?php
-                    //dates uniques en reverse
-                    
-                    ?>
                     <!-- Ajoutez d'autres options selon vos besoins -->
                 </select>
-                <select name="combo2" >
-                    <option >2023-12-19</option>
-                    <?php
-                    //dates uniques
-                    ?>
-                    <!-- Ajoutez d'autres options selon vos besoins -->   
+                <select name="combo2">
+                    <option>2023-12-19</option>
+                    <!-- Ajoutez d'autres options selon vos besoins -->
                 </select>
                 <input type='submit' value='Afficher'>
             </form>
             <br />
-            <div id="temperature"><b><?php echo "période étudiée";?></b></div>
+            <div id="temperature"><b><?php echo "période étudiée"; ?></b></div>
             <h1>Quelle température:</h1>
-            <p>Jettez un oeil à la température sur la période selectionnée.</p>
+            <p>Jetez un oeil à la température sur la période selectionnée.</p>
+
+            <h1>Quelle humidité:</h1>
+            <p>Jetez un oeil à l’humidité sur la période selectionnée.</p>
+            
+            <form method="get" action="http://api.localhost:9530/apirest.php">
+                <input type="hidden" name="resource" value="releves">
+                <input type="hidden" name="idSonde" value="1">
+                <input type="submit" value="Afficher le graphique">
+            </form>
+
 
             <table class="charts-css line show-primary-axis show-10-secondary-axes  show-labels  show-heading">
-
                 <tbody>
                     <?php
-
 
                     ?>
                 </tbody>
             </table>
-
-            <p>
-                <?php echo "La température moyenne entre ces dates est de "./*$lastDaysAvTemp*/ "°C"; ?>.
-            </p>
-            <?php //picto($lastDaysAvTemp)?>
-            <!-- <img src="../images/rire.svg" /> -->
         </div>
     </div>
 
